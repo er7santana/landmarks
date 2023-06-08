ec42a360f54c075922c71cce9e32aae5c9efc2a0
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2023-06-08 14:37:23 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
