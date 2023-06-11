acfe8b6ed5adbd84d50a11d815142c1557119c97
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2023-06-11 17:08:33 -0300

    creating PageControl do display featured images

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -16,17 +16,15 @@ struct CategoryHome: View {
         NavigationView {
             List {
                 
-                modelData.features[0].image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 200)
-                    .clipped()
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
+                    .aspectRatio(3 / 2, contentMode: .fit)
                     .listRowInsets(EdgeInsets())
                 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
                 .listRowInsets(EdgeInsets())
+                .listRowSeparator(.hidden)
             }
             .listStyle(.inset)
             .navigationTitle("Featured")
